6a5d7ce747859a13eb8dc7cd033d17e8088e3c6e
dave-br <dave-br@users.noreply.github.com>
2024-09-17 14:41:50 -0700

    lwasm now compiles with libdbginfo.a

--- src/lib/dbginfo/dbginfo.cpp
+++ src/lib/dbginfo/dbginfo.cpp
@@ -1,4 +1,19 @@
-int get_one()
+#include <stdio.h>
+#include "dbginfo.h"
+
+void * mame_mdi_simp_open_new(const char * file_path)
 {
-	return 1;
+	return NULL;
+}
+
+
+void mame_mdi_simp_add_line_mapping(void * mdi_simp_state, unsigned short address_first, unsigned short address_last, unsigned int line_number)
+{
+
+}
+
+
+void mame_mdi_simp_close(void * mdi_simp_state)
+{
+
 }
