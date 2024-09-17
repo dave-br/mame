6c10e7649db763c270b94e047078d5da769d9d14
dave-br <dave-br@users.noreply.github.com>
2024-09-17 15:34:04 -0700

    first draft written, not run

--- src/lib/dbginfo/dbginfo.h
+++ src/lib/dbginfo/dbginfo.h
@@ -3,7 +3,8 @@ extern "C" {
 #endif
 
 void * mame_mdi_simp_open_new(const char * file_path);
-void mame_mdi_simp_add_line_mapping(void * mdi_simp_state, unsigned short address_first, unsigned short address_last, unsigned int line_number);
+unsigned short mame_mdi_simp_add_source_file_path(void * mdi_simp_state, const char * source_file_path);
+void mame_mdi_simp_add_line_mapping(void * mdi_simp_state, unsigned short address_first, unsigned short address_last, unsigned short source_file_index, unsigned int line_number);
 void mame_mdi_simp_close(void * mdi_simp_state);
 
 #ifdef __cplusplus
