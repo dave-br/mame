080b327910b23286c85d30729f204fcb7112cb9a
dave-br <dave-br@users.noreply.github.com>
2024-09-18 11:08:38 -0700

    explicit calling convention, fix FILE member bug

--- src/lib/dbginfo/dbginfo.h
+++ src/lib/dbginfo/dbginfo.h
@@ -2,10 +2,10 @@
 extern "C" {
 #endif
 
-void * mame_mdi_simp_open_new(const char * file_path);
-unsigned short mame_mdi_simp_add_source_file_path(void * mdi_simp_state, const char * source_file_path);
-void mame_mdi_simp_add_line_mapping(void * mdi_simp_state, unsigned short address_first, unsigned short address_last, unsigned short source_file_index, unsigned int line_number);
-void mame_mdi_simp_close(void * mdi_simp_state);
+void * __cdecl mame_mdi_simp_open_new(const char * file_path);
+unsigned short __cdecl mame_mdi_simp_add_source_file_path(void * mdi_simp_state, const char * source_file_path);
+void __cdecl mame_mdi_simp_add_line_mapping(void * mdi_simp_state, unsigned short address_first, unsigned short address_last, unsigned short source_file_index, unsigned int line_number);
+void __cdecl mame_mdi_simp_close(void * mdi_simp_state);
 
 #ifdef __cplusplus
 }
