de2aa8019245e05da32984f4a2a54b9685fe461e
dave-br <dave-br@users.noreply.github.com>
2024-09-20 14:56:13 -0700

    source path support
    source prefix map and source search path

--- src/emu/debug/dvsourcecode.h
+++ src/emu/debug/dvsourcecode.h
@@ -44,16 +44,40 @@ struct file_line
 	u32 line_number;
 };
 
-
 // abstract base class for debug-info (symbols) file readers
 class debug_info_provider_base
 {
 public:
+	// TODO: MAKE FRIEND CLASS, AND ONLY GETTERS PUBLIC
+	class source_file_path
+	{
+	public:
+		source_file_path(std::string & built_p, std::string & local_p) :
+			m_built(std::move(built_p)),
+			m_local(std::move(local_p))
+		{
+		}
+
+		// TODO: VERIFY THIS IS CALLED WHEN SFP IS PLACED IN VECTOR
+		// source_file_path(source_file_path && sfp) :
+		// 	m_built(sfp.m_built),
+		// 	m_local(sfp.m_local)
+		// {
+		// }
+		
+		const char * built() const { return m_built.c_str(); }
+		const char * local() const { return m_local.c_str(); }  // TODO: WORKS IF LOCAL EMPTY?
+
+	private:
+		std::string m_built;
+		std::string m_local;
+	};
+
 	typedef std::pair<offs_t,offs_t> address_range;
 	static std::unique_ptr<debug_info_provider_base> create_debug_info(running_machine &machine);
 	virtual ~debug_info_provider_base() {};
 	virtual std::size_t num_files() const = 0;
-	virtual const char * file_index_to_path(u16 file_index) const = 0;
+	virtual const source_file_path & file_index_to_path(u16 file_index) const = 0;
 	virtual std::optional<int> file_path_to_index(const char * file_path) const = 0;
 	virtual std::optional<address_range> file_line_to_address_range (u16 file_index, u32 line_number) const = 0;
 	virtual std::optional<file_line> address_to_file_line (offs_t address) const = 0;
@@ -68,7 +92,7 @@ public:
 	debug_info_simple(running_machine& machine, std::vector<uint8_t>& data);
 	~debug_info_simple() { }
 	virtual std::size_t num_files() const override { return m_source_file_paths.size(); }
-	virtual const char * file_index_to_path(u16 file_index) const override { return m_source_file_paths[file_index]; };
+	virtual const source_file_path & file_index_to_path(u16 file_index) const override { return m_source_file_paths[file_index]; };
 	virtual std::optional<int> file_path_to_index(const char * file_path) const override;
 	virtual std::optional<address_range> file_line_to_address_range (u16 file_index, u32 line_number) const override;
 	virtual std::optional<file_line> address_to_file_line (offs_t address) const override;
@@ -81,8 +105,11 @@ private:
 		u32 line_number;
 	};
 
-	std::vector<char>                        m_source_file_path_chars; // Storage for source file path characters
-	std::vector<const char*>                 m_source_file_paths;      // Starting points for source file path strings
+	void generate_local_path(running_machine& machine, const std::string & built, std::string & local);
+	void apply_source_map(running_machine& machine, std::string & local);
+
+	// std::vector<char>                        m_source_file_path_chars; // Storage for source file path characters
+	std::vector<source_file_path>            m_source_file_paths;      // Starting points for source file path strings
 	std::vector<mdi_line_mapping>            m_linemaps_by_address;    // a list of mdi_line_mappings, sorted by address
 	std::vector<std::vector<address_line>>   m_linemaps_by_line;       // m_linemaps_by_line[i] is a list of address/line pairs,
 	                                                                   // sorted by line, from file #i
@@ -122,12 +149,13 @@ private:
 	void update_visible_lines(offs_t pc);
 	bool exists_bp_for_line(u16 src_index, u32 line);
 
-	device_state_interface *            m_state;                 // state interface, if present
-	const debug_info_provider_base &    m_debug_info;		     // Interface to the loaded debugging info file
-	u16                                 m_cur_src_index;         // Identifies which source file we should now show / switch to
-	u16                                 m_displayed_src_index;   // Identifies which source file is currently shown
-	std::unique_ptr<line_indexed_file>  m_displayed_src_file;    // File object currently printed to the view
-	u32                                 m_line_for_cur_pc;       // Line number to be highlighted
+	device_state_interface *                          m_state;                 // state interface, if present
+	const debug_info_provider_base &                  m_debug_info;            // Interface to the loaded debugging info file
+	u16                                               m_cur_src_index;         // Identifies which source file we should now show / switch to
+	u16                                               m_displayed_src_index;   // Identifies which source file is currently shown
+	std::unique_ptr<line_indexed_file>                m_displayed_src_file;    // File object currently printed to the view
+	std::vector<std::pair<std::string, std::string>>  m_src_path_map;
+	u32                                               m_line_for_cur_pc;       // Line number to be highlighted
 	// u32                                 m_first_visible_line;    // Line number to show at top of scrolled view
 };
 
