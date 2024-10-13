3aa4d3137bb2bc1d67c8aef420b2542e88cb17be
dave-br <dave-br@users.noreply.github.com>
2024-10-13 13:38:37 -0700

    more

--- src/emu/debug/srcdbg_format_reader.cpp
+++ src/emu/debug/srcdbg_format_reader.cpp
@@ -4,11 +4,10 @@
 
 ***************************************************************************/
 
-#include "mdisimple.h"
 #include "srcdbg_format_reader.h"
+#include "mdisimple.h"
 
 #include "corefile.h"
-
 
 #include <cstdio>
 
@@ -16,20 +15,35 @@ using u16 = uint16_t;
 using u32 = uint32_t;
 
 
-template <typename T> static bool read_field(T& var, std::vector<uint8_t>& data, u32& i)
+template <typename T> static bool read_field(const T & var, const std::vector<uint8_t> & data, u32 & i, std::string & error)
 {
 	if (data.size() < i + sizeof(T))
 	{
-		fprintf(stderr, "File ended prematurely while reading next field of size %u.\n", i);
+		error = "File ended prematurely while reading next field of size ";
+		error += sizeof(T);
 		return false;
 	}
 
-	var = *(T*) &data[i];
+	var = *(const T *) (data.data() + i);
 	i += sizeof(T);
 	return true;
 }
 
-int srcdbg_format_read(srcdbg_format_reader_callback & callback)
+static bool scan_bytes(u32 num_bytes, const std::vector<uint8_t>& data, u32& i, std::string & error)
+{
+	if (data.size() < i + num_bytes)
+	{
+		error = "File ended prematurely while scanning ahead ";
+		error += num_bytes;
+		error += " bytes.";
+		return false;
+	}
+
+	i += num_bytes;
+	return true;
+}
+
+bool srcdbg_format_read(srcdbg_format_reader_callback & callback, std::string & error)
 {
 	std::vector<uint8_t> data;
 
@@ -37,46 +51,56 @@ int srcdbg_format_read(srcdbg_format_reader_callback & callback)
 	u32 i = 0;
 	mame_debug_info_header_base header_base;
 	memset(&header_base, 0, sizeof(header_base));
-	read_field<mame_debug_info_header_base>(header_base, data, i);
+	if (!read_field<mame_debug_info_header_base>(header_base, data, i, error))
+	{
+		return false;
+	}
 	if (!callback.on_read_header_base(header_base))
 	{
-		return 0;
+		return true;
 	}
 
 	// Reread header as simple ('simp') type
 	i = 0;
 	mame_debug_info_simple_header header;
-	memset(&header, 0, sizeof(header));
-	read_field<mame_debug_info_simple_header>(header, data, i);
+	// memset(&header, 0, sizeof(header));
+	if (!read_field<mame_debug_info_simple_header>(header, data, i, error))
+	{
+		return false;
+	}
 	if (!callback.on_read_simp_header(header))
 	{
-		return 0;
+		return true;
 	}
 
 	u32 first_line_mapping = i + header.source_file_paths_size;
 	u32 first_symbol_name = first_line_mapping + (header.num_line_mappings * sizeof(mdi_line_mapping));
 	u32 after_symbol_names = first_symbol_name + header.symbol_names_size;
-	// if (header.symbol_names_size > 0)
-	// {
-	// 	if (data.size() <= first_symbol_address)
-	// 	{
-	// 		fprintf(stderr, "File too small to contain reported symbol_names_size=%u\n", header.symbol_names_size);
-	// 		return 1;
-	// 	}
+	if (header.symbol_names_size > 0)
+	{
+		if (data.size() <= after_symbol_names)
+		{
+			error = "File too small to contain reported symbol_names_size=";
+			error += header.symbol_names_size;
+			return false;
+		}
 
-	// 	if (data[first_symbol_address - 1] != '\0')
-	// 	{
-	// 		fprintf(stderr, "null terminator missing at end of last symbol name\n");
-	// 		return 1;
-	// 	}
-	// }
+		if (data[after_symbol_names - 1] != '\0')
+		{
+			error = "null terminator missing at end of last symbol name";
+			return false;
+		}
+	}
 	std::string str;
 	u16 source_index = 0;
 	for (; i < first_line_mapping; i++)
 	{
 		if (data[i] == '\0')
 		{
-			callback.on_read_source_path(source_index++, std::move(str));
+			if (!callback.on_read_source_path(source_index++, std::move(str)))
+			{
+				return true;
+			}
 			str.clear();
 		}
 		else
@@ -88,9 +112,15 @@ int srcdbg_format_read(srcdbg_format_reader_callback & callback)
 	for (u32 line_map_idx = 0; line_map_idx < header.num_line_mappings; line_map_idx++)
 	{
 		mdi_line_mapping line_map;
-		memset(&line_map, 0, sizeof(line_map));
-		read_field<mdi_line_mapping>(line_map, data, i);
-		callback.on_read_line_mapping(line_map);
+		// memset(&line_map, 0, sizeof(line_map));
+		if (!read_field<mdi_line_mapping>(line_map, data, i, error))
+		{
+			return false;
+		}
+		if (!callback.on_read_line_mapping(line_map))
+		{
+			return true;
+		}
 	}
 
 	u16 symbol_index = 0;
@@ -98,7 +128,10 @@ int srcdbg_format_read(srcdbg_format_reader_callback & callback)
 	{
 		if (data[i] == '\0')
 		{
-			callback.on_read_symbol_name(symbol_index++, std::move(str));
+			if (!callback.on_read_symbol_name(symbol_index++, std::move(str)))
+			{
+				return true;
+			}
 			str.clear();
 		}
 		else
@@ -107,17 +140,54 @@ int srcdbg_format_read(srcdbg_format_reader_callback & callback)
 		}
 	}
 
-	u32 j = first_symbol_address;
-	for (; i < first_symbol_address; i++)
+	for (u32 global_constant_idx = 0; global_constant_idx < header.num_global_constant_symbol_values; global_constant_idx++)
 	{
-		if (data[i] == '\0')
+		global_constant_symbol_value value;
+		// memset(&value, 0, sizeof(value));
+		if (!read_field<global_constant_symbol_value>(value, data, i, error))
 		{
-			printf("\t%d\n", *(int*) &data[j]);
-			j += sizeof(int);
+			return false;
 		}
-		else
+		if (!callback.on_read_global_constant_symbol_value(value))
 		{
-			printf("%c", data[i]);
+			return true;
 		}
 	}
+
+	u32 after_local_constant_symbol_values = i + header.local_constant_symbol_values_size;
+	while (i < after_local_constant_symbol_values)
+	{
+		local_constant_symbol_value value;
+		u32 value_start_idx = i;
+		// memset(&value, 0, sizeof(value));
+		if (!read_field<local_constant_symbol_value>(value, data, i, error) ||
+			!scan_bytes(value.num_address_ranges * sizeof(address_range), data, i, error))
+		{
+			return false;
+		}
+		if (!callback.on_read_local_constant_symbol_value(*(const local_constant_symbol_value *) &data[value_start_idx]))
+		{
+			return true;
+		}
+	}
+
+	u32 after_local_dynamic_symbol_values = i + header.local_dynamic_symbol_values_size;
+	while (i < after_local_dynamic_symbol_values)
+	{
+		local_dynamic_symbol_value value;
+		u32 value_start_idx = i;
+		// memset(&value, 0, sizeof(value));
+		if (!read_field<local_dynamic_symbol_value>(value, data, i, error) ||
+			!scan_bytes(value.num_local_dynamic_symbol_entries * sizeof(local_dynamic_symbol_entry), data, i, error))
+		{
+			return false;
+		}
+		if (!callback.on_read_local_dynamic_symbol_value(*(const local_dynamic_symbol_value *) &data[value_start_idx]))
+		{
+			return true;
+		}
+	}
+
+	// TODO: MORE CHECKING OF HEADER SIZE VALS AND NUM_SYMS FOR LOCS
+	return true;
 }
