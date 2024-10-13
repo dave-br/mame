f0788b9126ee447d75c77fbe7403ab3ac8a3eec2
dave-br <dave-br@users.noreply.github.com>
2024-10-12 19:39:13 -0700

    more reading

--- src/emu/debug/srcdbg_format_reader.cpp
+++ src/emu/debug/srcdbg_format_reader.cpp
@@ -5,8 +5,10 @@
 ***************************************************************************/
 
 #include "mdisimple.h"
+#include "srcdbg_format_reader.h"
 
 #include "corefile.h"
+
 
 #include <cstdio>
 
@@ -27,109 +29,84 @@ template <typename T> static bool read_field(T& var, std::vector<uint8_t>& data,
 	return true;
 }
 
-int main(int argc, char *argv[])
+int srcdbg_format_read(srcdbg_format_reader_callback & callback)
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage:\ndbginfodump <path to mdi file>\n");
-		return 1;
-	}
-
-	printf("Dumping '%s'...\n", argv[1]);
 	std::vector<uint8_t> data;
-	util::core_file::load(argv[1], data);
 
 	// Read base header first to detemine type
-
 	u32 i = 0;
 	mame_debug_info_header_base header_base;
 	memset(&header_base, 0, sizeof(header_base));
 	read_field<mame_debug_info_header_base>(header_base, data, i);
-
-	printf("magic:\t'%.4s'\n", header_base.magic);
-	printf("type:\t'%.4s'\n", header_base.type);
-	
-	if (strncmp(header_base.type, "simp", 4) != 0)
+	if (!callback.on_read_header_base(header_base))
 	{
-		fprintf(stderr, "Only type 'simp' is currently supported.\n");
-		return 1;
-	};
-	
-	printf("type:\t%d\n", (int) header_base.version);
-	
-	if (header_base.version != 1)
-	{
-		fprintf(stderr, "Only version 1 is currently supported.\n");
-		return 1;
-	};
+		return 0;
+	}
 
 	// Reread header as simple ('simp') type
-	printf("\nReading simple format...\n\n");
 	i = 0;
 	mame_debug_info_simple_header header;
 	memset(&header, 0, sizeof(header));
 	read_field<mame_debug_info_simple_header>(header, data, i);
-
-	printf("source_file_paths_size:\t%u\n", header.source_file_paths_size);
-	printf("num_line_mappings:\t%u\n", header.num_line_mappings);
+	if (!callback.on_read_simp_header(header))
+	{
+		return 0;
+	}
 
 	u32 first_line_mapping = i + header.source_file_paths_size;
-	if (data.size() <= first_line_mapping - 1)
-	{
-		fprintf(stderr, "File too small to contain reported source_file_paths_size=%u\n", header.source_file_paths_size);
-		return 1;
-	}
-	if (data[first_line_mapping - 1] != '\0')
-	{
-		fprintf(stderr, "null terminator missing at end of last source file\n");
-		return 1;
-	}
-
 	u32 first_symbol_name = first_line_mapping + (header.num_line_mappings * sizeof(mdi_line_mapping));
-	u32 first_symbol_address = first_symbol_name + header.symbol_names_size;
-	if (header.symbol_names_size > 0)
-	{
-		if (data.size() <= first_symbol_address)
-		{
-			fprintf(stderr, "File too small to contain reported symbol_names_size=%u\n", header.symbol_names_size);
-			return 1;
-		}
+	u32 after_symbol_names = first_symbol_name + header.symbol_names_size;
+	// if (header.symbol_names_size > 0)
+	// {
+	// 	if (data.size() <= first_symbol_address)
+	// 	{
+	// 		fprintf(stderr, "File too small to contain reported symbol_names_size=%u\n", header.symbol_names_size);
+	// 		return 1;
+	// 	}
 
-		if (data[first_symbol_address - 1] != '\0')
-		{
-			fprintf(stderr, "null terminator missing at end of last symbol name\n");
-			return 1;
-		}
-	}
-
-	printf("\n**** Source file paths: ****\n");
+	// 	if (data[first_symbol_address - 1] != '\0')
+	// 	{
+	// 		fprintf(stderr, "null terminator missing at end of last symbol name\n");
+	// 		return 1;
+	// 	}
+	// }
+	std::string str;
 	u16 source_index = 0;
 	for (; i < first_line_mapping; i++)
 	{
 		if (data[i] == '\0')
 		{
-			printf(" (index %u)\n", source_index++);
+			callback.on_read_source_path(source_index++, std::move(str));
+			str.clear();
 		}
 		else
 		{
-			printf("%c", data[i]);
+			str += char(data[i]);
 		}
 	}
 
-	printf("\n**** Line mappings: ****\n");
 	for (u32 line_map_idx = 0; line_map_idx < header.num_line_mappings; line_map_idx++)
 	{
 		mdi_line_mapping line_map;
 		memset(&line_map, 0, sizeof(line_map));
 		read_field<mdi_line_mapping>(line_map, data, i);
-		printf("address_first: $%X\taddress_last: $%X\tsource_file_index: %u\tline_number: %u\n",
-			u32(line_map.address_first),
-			u32(line_map.address_last),
-			u32(line_map.source_file_index),
-			line_map.line_number);
+		callback.on_read_line_mapping(line_map);
 	}
 
-	printf("\n**** Symbols: ****\n");
+	u16 symbol_index = 0;
+	for (; i < after_symbol_names; i++)
+	{
+		if (data[i] == '\0')
+		{
+			callback.on_read_symbol_name(symbol_index++, std::move(str));
+			str.clear();
+		}
+		else
+		{
+			str += char(data[i]);
+		}
+	}
+
 	u32 j = first_symbol_address;
 	for (; i < first_symbol_address; i++)
 	{
