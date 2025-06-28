e716fc80cb844d9701406f49c2dfcc04024fd0b1
Vas Crabb <vas@vastheman.com>
2025-06-29 06:55:26 +1000

    Moved preview changes from 0287 to 0288

--- src/emu/emufwd.h
+++ src/emu/emufwd.h
@@ -84,6 +84,14 @@ class debug_watchpoint;
 class debug_registerpoint;
 class debug_exceptionpoint;
 
+// declared in debug/srcdbg_provider.h
+class srcdbg_provider_base;
+class file_line;
+
+// declared in debug/srcdbg_info.h
+class line_indexed_file;
+class srcdbg_info;
+
 // declared in debugger.h
 class debugger_manager;
 
@@ -143,6 +151,7 @@ class emu_options;
 
 // declared in fileio.h
 class emu_file;
+class path_iterator;
 
 // declared in http.h
 class http_manager;
